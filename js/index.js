6772a82f64b7646d5a05557895e32a7e1ddf8015
Omnified Tech <omnifiedtech@gmail.com>
2018-03-17 19:48:22 +0000

    update site with new navbar

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
 AOS.init();
 
+window.onscroll = () => scrollNav();
 
 function scrollToDiv(id){
     document.getElementById(id).scrollIntoView({block: 'start', behavior: 'smooth'});
+}
+
+function scrollNav(){
+    // if the scroll bar is below 20px, show the navbar.
+    // dirty ternary here because I liked how it looked.
+    // preemptive minification (fewer chars & loc)
+    document.getElementById('nav').style.top = 
+        (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) ?
+        '0px' : '-60px';
+        
 }
 
 
