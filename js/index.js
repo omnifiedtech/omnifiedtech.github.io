2a6e2998219e8b74a64ee89d071e359e37902207
Omnified Tech <omnifiedtech@gmail.com>
2018-03-30 03:03:32 +0000

    restart site with new aesthetic

--- js/index.js
+++ js/index.js
@@ -1,57 +1,8 @@
-
-//nav lookups
-var elements = [document.getElementById('whoarewe'), document.getElementById('contact')];
-var elementsLi = [document.getElementById('whoareweLi'), document.getElementById('contactLi')];
-var li = [];
-var currentSection = undefined;
-
-// element lookup pointers
-var nav = document.getElementById('nav');
-
-
-
-// init function
-(function() {
-    AOS.init();
-    window.onscroll = () => scrollNav();
-
-    // create a LUT for offsets
-    for(i in elements){
-        li.push(getOffset(elements[i]));
-    }
-    
-    
+// init
+(() => {
+    AOS.init({
+        duration: 400,
+        easing: 'ease-in-linear',
+        delay: 100,
+    });
 })();
-
-function scrollToDiv(id){
-    document.getElementById(id).scrollIntoView({block: 'start', behavior: 'smooth'});
-}
-
-function scrollNav(){
-    // if the scroll bar is below 20px, show the navbar.
-    // dirty ternary here because I liked how it looked.
-    // preemptive minification (fewer chars & loc)
-    nav.style.top = 
-        (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) ?
-        '0px' : '-60px';
-
-    
-    /*handle scroll highlighting*/
-    if(window.pageYOffset < li[0] - 250){
-        elementsLi[0].classList.remove("active");
-    }
-    
-    if(window.pageYOffset > li[1] - 500){
-        elementsLi[1].classList.add("active");
-        elementsLi[0].classList.remove("active");
-    } else if(window.pageYOffset > li[0] - 350){
-        elementsLi[0].classList.add("active");
-        elementsLi[1].classList.remove("active");
-    }
-    
-}
-
-// get offset of element from top
-function getOffset(element){
-    return (element.getBoundingClientRect().top + window.scrollY);
-}
