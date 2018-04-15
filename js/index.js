b9ff547debde7ecc6bf61b8abb4672d396e590ec
Simon mahns <ak.simonm@gmail.com>
2018-04-14 23:32:01 -0400

    updates

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
 // init
-(() => {
+var window_width = $(window).width() - $('#rocket').width();
 
-})();
+var document_height = $(document).height() - $(window).height();
+
+$(function () {
+    $(window).scroll(function () {
+        var scroll_position = $(window).scrollTop();
+        var object_position_left = window_width * (scroll_position / document_height);
+        $('#rocket').css({
+            'left': object_position_left
+        });
+    });
+});
